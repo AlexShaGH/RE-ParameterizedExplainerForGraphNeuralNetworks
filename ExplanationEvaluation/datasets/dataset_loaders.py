51d1225b723bd57bad174ea8b66f0011db63873d
Shubham Modi <shubhammodi.11@gmail.com>
2022-04-24 20:31:50 -0500

    Twitch Dataset GNN Explainer

--- ExplanationEvaluation/datasets/dataset_loaders.py
+++ ExplanationEvaluation/datasets/dataset_loaders.py
@@ -1,82 +1,74 @@
-import pickle as pkl
 import numpy as np
-import os
-from numpy.random.mtrand import RandomState
-
-from ExplanationEvaluation.datasets.utils import preprocess_features, preprocess_adj, adj_to_edge_index, load_real_dataset
+import torch
+from torch_geometric.datasets import Twitch
 
 
-def load_graph_dataset(_dataset, shuffle=True):
-    """Load a graph dataset and optionally shuffle it.
+# def load_FacebookPagePageDataset():
+#     rawPath = os.path.dirname(
+#         os.path.realpath(__file__))
+#     dataset = FacebookPagePage(root=rawPath+"/facebook")
+#     data = dataset.data
+#     adj = to_dense_adj(data.edge_index)[0].numpy()
+#     features = data.x.numpy()
+#     n_graphs = adj.shape[0]
+#     labels = np.eye(dataset.num_classes, dtype='uint8')[data.y.numpy()]
+#     train_indices = np.arange(0, int(n_graphs*0.8))
+#     val_indices = np.arange(int(n_graphs*0.8), int(n_graphs*0.9))
+#     test_indices = np.arange(int(n_graphs*0.9), n_graphs)
+#     train_mask = np.full((n_graphs), False, dtype=bool)
+#     train_mask[train_indices] = True
+#     val_mask = np.full((n_graphs), False, dtype=bool)
+#     val_mask[val_indices] = True
+#     test_mask = np.full((n_graphs), False, dtype=bool)
+#     test_mask[test_indices] = True
 
-    :param _dataset: Which dataset to load. Choose from "ba2" or "mutag"
-    :param shuffle: Boolean. Wheter to suffle the loaded dataset.
-    :returns: np.array
-    """
-    # Load the chosen dataset from the pickle file.
-    if _dataset == "ba2":
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        path = dir_path + '/pkls/' + "BA-2motif" + '.pkl'
-        with open(path, 'rb') as fin:
-            adjs, features, labels = pkl.load(fin)
-
-    elif _dataset == "mutag":
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        path = dir_path + '/pkls/' + "Mutagenicity" + '.pkl'
-        if not os.path.exists(path): # pkl not yet created
-            print("Mutag dataset pickle is not yet created, doing this now. Can take some time")
-            adjs, features, labels = load_real_dataset(path, dir_path + '/Mutagenicity/Mutagenicity_')
-            print("Done with creating the mutag dataset")
-        else:
-            with open(path, 'rb') as fin:
-                adjs, features, labels = pkl.load(fin)
-    else:
-        print("Unknown dataset")
-        raise NotImplemented
-
-    n_graphs = adjs.shape[0]
-    indices = np.arange(0, n_graphs)
-    if shuffle:
-        prng = RandomState(42) # Make sure that the permutation is always the same, even if we set the seed different
-        indices = prng.permutation(indices)
-
-    # Create shuffled data
-    adjs = adjs[indices]
-    features = features[indices].astype('float32')
-    labels = labels[indices]
-
-    # Create masks
-    train_indices = np.arange(0, int(n_graphs*0.8))
-    val_indices = np.arange(int(n_graphs*0.8), int(n_graphs*0.9))
-    test_indices = np.arange(int(n_graphs*0.9), n_graphs)
-    train_mask = np.full((n_graphs), False, dtype=bool)
-    train_mask[train_indices] = True
-    val_mask = np.full((n_graphs), False, dtype=bool)
-    val_mask[val_indices] = True
-    test_mask = np.full((n_graphs), False, dtype=bool)
-    test_mask[test_indices] = True
-
-    # Transform to edge index
-    edge_index = adj_to_edge_index(adjs)
-
-    return edge_index, features, labels, train_mask, val_mask, test_mask
+#     return data.edge_index, features, labels, train_mask, val_mask, test_mask
 
 
-def _load_node_dataset(_dataset):
-    """Load a node dataset.
+# def _load_node_dataset(_dataset):
+#     """Load a node dataset.
 
-    :param _dataset: Which dataset to load. Choose from "syn1", "syn2", "syn3" or "syn4"
-    :returns: np.array
-    """
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    path = dir_path + '/pkls/' + _dataset + '.pkl'
-    with open(path, 'rb') as fin:
-        adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, edge_label_matrix  = pkl.load(fin)
-    labels = y_train
-    labels[val_mask] = y_val[val_mask]
-    labels[test_mask] = y_test[test_mask]
+#     :param _dataset: Which dataset to load. Choose from "syn1", "syn2", "syn3" or "syn4"
+#     :returns: np.array
+#     """
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#     path = dir_path + '/pkls/' + _dataset + '.pkl'
+#     with open(path, 'rb') as fin:
+#         adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, edge_label_matrix = pkl.load(
+#             fin)
+#     labels = y_train
+#     labels[val_mask] = y_val[val_mask]
+#     labels[test_mask] = y_test[test_mask]
 
-    return adj, features, labels, train_mask, val_mask, test_mask
+#     return adj, features, labels, train_mask, val_mask, test_mask
+
+
+# def load_politifact_dataset():
+#     rawPath = os.path.dirname(
+#         os.path.realpath(__file__))
+#     dataset = FNNDataset(root=rawPath, feature="content",
+#                          empty=False, name="politifact", transform=ToUndirected())
+
+#     graph = dataset.data
+#     print(graph)
+#     # Add train and test masks
+#     num_nodes = graph.x.shape[0]
+#     master = np.arange(0, num_nodes)
+#     np.random.shuffle(master)
+#     train = torch.zeros(num_nodes)
+#     train[master[2000:]] = 1
+#     train_mask = train.bool()
+
+#     val = torch.zeros(num_nodes)
+#     val[master[2001:3000]] = 1
+#     val_mask = val.bool()
+
+#     test = torch.zeros(num_nodes)
+#     test[master[3001: num_nodes]] = 1
+#     test_mask = test.bool()
+
+#     return graph.edge_index, graph.x, graph.y.numpy(), train_mask, val_mask, test_mask
+#     return adjs, features, labels, train_mask, val_mask, test_mask
 
 
 def load_dataset(_dataset, skip_preproccessing=False, shuffle=True):
@@ -90,14 +82,61 @@ def load_dataset(_dataset, skip_preproccessing=False, shuffle=True):
     :returns: multiple np.arrays
     """
     print(f"Loading {_dataset} dataset")
-    if _dataset[:3] == "syn": # Load node_dataset
-        adj, features, labels, train_mask, val_mask, test_mask = _load_node_dataset(_dataset)
-        preprocessed_features = preprocess_features(features).astype('float32')
-        if skip_preproccessing:
-            graph = adj
-        else:
-            graph = preprocess_adj(adj)[0].astype('int64').T
-        labels = np.argmax(labels, axis=1)
-        return graph, preprocessed_features, labels, train_mask, val_mask, test_mask
-    else: # Load graph dataset
-        return load_graph_dataset(_dataset, shuffle)
+    # if _dataset == "facebook":
+    #     return load_FacebookPagePageDataset()
+    # if _dataset == "webKB":
+    #     return load_WebKBDataset()
+    if _dataset == "Twitch":
+        return load_TwitchDataset()
+    # if _dataset == "politifact":
+    #     return load_politifact_dataset()
+    else:
+        raise NotImplementedError
+
+
+# def load_WebKBDataset():
+#     dataset = WebKB(
+#         "/Users/shubhammodi/Documents/CS 579/Project 2/RE-ParameterizedExplainerForGraphNeuralNetworks/ExplanationEvaluation/datasets/WebKB", "cornell")
+#     adjs = to_dense_adj(dataset.data.edge_index)[0]
+#     labels = np.eye(dataset.num_classes, dtype='uint8')[
+#         dataset.data.y.numpy()]
+
+#     n_graphs = adjs.shape[0]
+#     indices = np.arange(0, n_graphs)
+#     prng = RandomState(183)
+#     indices = prng.permutation(indices)
+
+#     # Create masks
+#     train_indices = np.arange(0, int(n_graphs*0.7))
+#     val_indices = np.arange(int(n_graphs*0.7), int(n_graphs*0.8))
+#     test_indices = np.arange(int(n_graphs*0.8), n_graphs)
+#     train_mask = np.full((n_graphs), False, dtype=bool)
+#     train_mask[train_indices] = True
+#     val_mask = np.full((n_graphs), False, dtype=bool)
+#     val_mask[val_indices] = True
+#     test_mask = np.full((n_graphs), False, dtype=bool)
+#     test_mask[test_indices] = True
+
+#     return dataset.data.edge_index, dataset.data.x, dataset.data.y, train_mask, val_mask, test_mask
+
+
+def load_TwitchDataset():
+    graph = Twitch(root="/Users/shubhammodi/Documents/CS 579/Project 2/RE-ParameterizedExplainerForGraphNeuralNetworks/ExplanationEvaluation/datasets/Twitch", name="EN")
+    graph = graph.data
+    # Add train and test masks
+    num_nodes = graph.x.shape[0]
+    master = np.arange(0, num_nodes)
+    np.random.shuffle(master)
+    train = torch.zeros(num_nodes)
+    train[master[2000:]] = 1
+    train_mask = train.bool()
+
+    val = torch.zeros(num_nodes)
+    val[master[2001:3000]] = 1
+    val_mask = val.bool()
+
+    test = torch.zeros(num_nodes)
+    test[master[3001: num_nodes]] = 1
+    test_mask = test.bool()
+
+    return graph.edge_index, graph.x, graph.y.numpy(), train_mask, val_mask, test_mask
--- ExplanationEvaluation/models/model_selector.py
+++ ExplanationEvaluation/models/model_selector.py
@@ -1,48 +1,19 @@
+from email.policy import strict
 import torch
 import os
 
 from ExplanationEvaluation.models.GNN_paper import NodeGCN as GNN_NodeGCN
-from ExplanationEvaluation.models.GNN_paper import GraphGCN as GNN_GraphGCN
-from ExplanationEvaluation.models.PG_paper import NodeGCN as PG_NodeGCN
-from ExplanationEvaluation.models.PG_paper import GraphGCN as PG_GraphGCN
 
-def string_to_model(paper, dataset):
+
+def string_to_model(dataset):
     """
     Given a paper and a dataset return the cooresponding neural model needed for training.
     :param paper: the paper who's classification model we want to use.
     :param dataset: the dataset on which we wish to train. This ensures that the model in- and output are correct.
     :returns: torch.nn.module models
     """
-    if paper == "GNN":
-        if dataset in ['syn1']:
-            return GNN_NodeGCN(10, 4)
-        elif dataset in ['syn2']:
-            return GNN_NodeGCN(10, 8)
-        elif dataset in ['syn3']:
-            return GNN_NodeGCN(10, 2)
-        elif dataset in ['syn4']:
-            return GNN_NodeGCN(10, 2)
-        elif dataset == "ba2":
-            return GNN_GraphGCN(10, 2)
-        elif dataset == "mutag":
-            return GNN_GraphGCN(14, 2)
-        else:
-            raise NotImplementedError
-    elif paper == "PG":
-        if dataset in ['syn1']:
-            return PG_NodeGCN(10, 4)
-        elif dataset in ['syn2']:
-            return PG_NodeGCN(10, 8)
-        elif dataset in ['syn3']:
-            return PG_NodeGCN(10, 2)
-        elif dataset in ['syn4']:
-            return PG_NodeGCN(10, 2)
-        elif dataset == "ba2":
-            return PG_GraphGCN(10, 2)
-        elif dataset == "mutag":
-            return PG_GraphGCN(14, 2)
-        else:
-            raise NotImplementedError
+    if dataset == "Twitch":
+        return GNN_NodeGCN(128, 2)
     else:
         raise NotImplementedError
 
@@ -65,15 +36,18 @@ def model_selector(paper, dataset, pretrained=True, return_checkpoint=False):
     :param paper: the paper who's classification model we want to use.
     :param dataset: the dataset on which we wish to train. This ensures that the model in- and output are correct.
     :param pretrained: whter to return a pre-trained model or not.
-    :param return_checkpoint: wheter to return the dict contining the models parameters or not.
+    :param return_checkpoint: whether to return the dict contining the models parameters or not.
     :returns: torch.nn.module models and optionallly a dict containing it's parameters.
     """
-    model = string_to_model(paper, dataset)
+    model = string_to_model(dataset)
     if pretrained:
         path = get_pretrained_path(paper, dataset)
+        # print(path)
         checkpoint = torch.load(path)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        print(f"This model obtained: Train Acc: {checkpoint['train_acc']:.4f}, Val Acc: {checkpoint['val_acc']:.4f}, Test Acc: {checkpoint['test_acc']:.4f}.")
+        # print(checkpoint["model_state_dict"])
+        model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+        print(
+            f"This model obtained: Train Acc: {checkpoint['train_acc']:.4f}, Val Acc: {checkpoint['val_acc']:.4f}, Test Acc: {checkpoint['test_acc']:.4f}.")
         if return_checkpoint:
             return model, checkpoint
     return model
